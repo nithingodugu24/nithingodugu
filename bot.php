9c384969920a8413ad59f8782a3c3b5f5b15668f
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-01-04 17:50:03 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -545,29 +545,30 @@ Checked By @$username ***");
         send_message($chat_id,$message_id, "Invalid Input");
     }
 }
+$API_TOKEN = "";
 ///Send Message (Global)
     function send_message($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['API_TOKEN'];  
+        $apiToken = $API_TOKEN';  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");
     }
     
 //Send Messages with Markdown (Global)
       function send_MDmessage($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['API_TOKEN'];  
+        $apiToken = $API_TOKEN;  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
     }
 ///Send Message to Channel
       function send_Cmessage($channel_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['API_TOKEN'];
+        $apiToken = $API_TOKEN;
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
     }
 
 //Send Dice (dynamic emoji)
 function sendDice($chat_id,$message_id, $message){
-        $apiToken = $_ENV['API_TOKEN'];  
+        $apiToken = $API_TOKEN;  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&reply_to_message_id=$message_id&text=$message");
     }
 
