5f8afc3b23ec3e2c652c160f03c342e9e05d8393
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-01-06 19:27:58 +0530

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,9 @@
+<?php
+
+
+    
+        $text = "hfggg";
+
+          $apiToken = '5249462923:AAEquCkhHvyVmqMfLZHSLq6DC9kV0G7COjI';
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=973566639&text=$text");
+    ?>
