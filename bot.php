c3a01ae3d51015554d438b83291de1577372a86e
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-01-06 19:30:33 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 
     
-        $text = "hfggg";
+        $text =$postdata = file_get_contents("php://input");
 
           $apiToken = '5249462923:AAEquCkhHvyVmqMfLZHSLq6DC9kV0G7COjI';
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=973566639&text=$text");
