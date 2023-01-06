b6327a4bdcc4db4789d8f2dc93f16c77841d795f
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-01-06 19:32:55 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,16 @@
 <?php
 
-
+date_default_timezone_set("Asia/kolkata");
+    //Data From Webhook
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chat_id = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+    $message_id = $update["message"]["message_id"];
+    $id = $update["message"]["from"]["id"];
+    $username = $update["message"]["from"]["username"];
+    $firstname = $update["message"]["from"]["first_name"];
+    $chatname = '@vmrmecattbot';
     
         $text =$postdata = file_get_contents("php://input");
 
