f6dfe9ce3ac4f6e80c04d74156e1872b62bad106
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-01-09 17:47:19 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -39,6 +39,9 @@ $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $resp = curl_exec($curl);
 curl_close($curl);
